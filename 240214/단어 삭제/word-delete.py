b6e5dcd228a1bb1d10a5905bf5b1a293ca5fd6ef
ChanYoung <91073404+Jneck@users.noreply.github.com>
2024-02-14 18:47:15 +0900

    Title: 단어 삭제, Time: 161ms, Memory: 25MB, Status: Passed - CodeTree

--- 240214/단어 삭제/word-delete.py
+++ 240214/단어 삭제/word-delete.py
@@ -3,22 +3,27 @@ print = sys.stdout.write
 tmp_str = input()
 erase_str = input()
 erase_len = len(erase_str)
-idx = 0
 from collections import deque
 
 dq = deque()
+idx = erase_len-1
 for tmp_char in tmp_str:
     dq.append(tmp_char)
-    if tmp_char != erase_str[idx]:
-        idx = 0
-        continue
-    elif erase_len == idx+1:
-        for i in range(erase_len):
-            dq.pop()
-        idx = 0
-        continue
-    else:
-        idx += 1
+    # 끝자리가 같으면 검증 시작
+    if tmp_char == erase_str[idx]:
+        tmp_dq = deque()
+        # idx가 넘지 않을때 끝이 동일하면 tmp_dq에 저장
+        while idx != -1 and dq and dq[-1] == erase_str[idx]:
+            tmp_dq.append(dq.pop())
+            idx -= 1
+        # 만약에 다 맞지 않으면 다시 넣어주기
+        if idx != -1:
+            while tmp_dq:
+                dq.append(tmp_dq.pop())
+        idx = erase_len-1
+        
+            
+
 
 while dq:
     print(dq.popleft())
